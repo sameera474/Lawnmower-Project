48e34fa9bf42ed89109fce3c304eb8972938e96d
Sameera Chaturanga Wagaarachchige <sameera474@gmail.com>
2024-11-13 17:53:53 +0200

    update old server

--- backend/server.js
+++ backend/server.js
@@ -9,25 +9,25 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware for CORS and JSON parsing
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3000",
-//       "https://lawnmower-project-frontend.vercel.app",
-//     ],
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:3000",
+      "https://lawnmower-project-frontend.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 
 // MongoDB connection
